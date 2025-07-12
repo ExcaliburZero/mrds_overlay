a4f48e5fe5e72c933bf8499ce582a80d94302ec0
Christopher Wells <cwellsny@nycap.rr.com>
2025-07-12 11:53:27 -0700

    Format code

--- overlay.lua
+++ overlay.lua
@@ -20,15 +20,15 @@ local changes_remaining_duration = 0
 local changes_previous_monster = {
     ["stress"] = -1,
     ["fatigue"] = -1,
-    ["lifespan"] = -1,
+    ["lifespan"] = -1
 }
 
 function display_monster_changes(current_time, monster)
-    --gui.text(1, 5, current_time)
-    --gui.text(1, 15, changes_remaining_duration)
+    -- gui.text(1, 5, current_time)
+    -- gui.text(1, 15, changes_remaining_duration)
 
-    --gui.text(1, 35, changes_previous_monster["stress"])
-    --gui.text(1, 45, monster["stress"])
+    -- gui.text(1, 35, changes_previous_monster["stress"])
+    -- gui.text(1, 45, monster["stress"])
 
     if changes_last_update == 0 or current_time - changes_last_update > 1 then
         changes_last_update = current_time
@@ -44,12 +44,13 @@ function display_monster_changes(current_time, monster)
     if changes_remaining_duration > 0 then
     elseif changes_previous_monster["stress"] == -1 then
         changes_previous_monster = monster
-    elseif changes_previous_monster["stress"] ~= monster["stress"] or changes_previous_monster["fatigue"] ~= monster["fatigue"] or changes_previous_monster["lifespan"] ~= monster["lifespan"] then
+    elseif changes_previous_monster["stress"] ~= monster["stress"] or changes_previous_monster["fatigue"] ~=
+        monster["fatigue"] or changes_previous_monster["lifespan"] ~= monster["lifespan"] then
         changes_remaining_duration = changes_display_duration
     end
 
     if changes_remaining_duration > 0 then
-        --gui.text(1, 25, "Displaying")
+        -- gui.text(1, 25, "Displaying")
 
         if changes_previous_monster["stress"] ~= monster["stress"] then
             if monster["stress"] > changes_previous_monster["stress"] then
@@ -61,23 +62,26 @@ function display_monster_changes(current_time, monster)
 
         if changes_previous_monster["fatigue"] ~= monster["fatigue"] then
             if monster["fatigue"] > changes_previous_monster["fatigue"] then
-                gui.text(1, 172, string.format("         +%d", monster["fatigue"] - changes_previous_monster["fatigue"]), "green")
+                gui.text(1, 172,
+                    string.format("         +%d", monster["fatigue"] - changes_previous_monster["fatigue"]), "green")
             else
-                gui.text(1, 172, string.format("         %d", monster["fatigue"] - changes_previous_monster["fatigue"]), "red")
+                gui.text(1, 172, string.format("         %d", monster["fatigue"] - changes_previous_monster["fatigue"]),
+                    "red")
             end
         end
 
         if changes_previous_monster["lifespan"] ~= monster["lifespan"] then
             if monster["lifespan"] > changes_previous_monster["lifespan"] then
-                gui.text(1, 172, string.format("                +%d", monster["lifespan"] - changes_previous_monster["lifespan"]), "green")
+                gui.text(1, 172, string.format("                +%d",
+                    monster["lifespan"] - changes_previous_monster["lifespan"]), "green")
             else
-                gui.text(1, 172, string.format("                %d", monster["lifespan"] - changes_previous_monster["lifespan"]), "red")
+                gui.text(1, 172, string.format("                %d",
+                    monster["lifespan"] - changes_previous_monster["lifespan"]), "red")
             end
         end
     end
 
 end
-
 
 function main()
     current_time = os.time()
@@ -87,5 +91,5 @@ function main()
     display_monster_changes(current_time, current_monster)
 end
 
---current_monster = read_current_monster()
+-- current_monster = read_current_monster()
 gui.register(main)
